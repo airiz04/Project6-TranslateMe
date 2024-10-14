aa13fe5564c8469931392a6582cb7a9cea4588f5
Anthony Irizarry <airiz04@outlook.com>
2024-10-14 19:30:12 -0400

    Initial Commit

---
+++ Project6 TranslateMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project6 TranslateMe
+//
+//  Created by Anthony Irizarry on 10/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project6 TranslateMe/Project6_TranslateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project6_TranslateMeApp.swift
+//  Project6 TranslateMe
+//
+//  Created by Anthony Irizarry on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Project6_TranslateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
